109082f8e734d0aa794a03a1f28dc659ae409bae
Luis Padron <heyluispadron@gmail.com>
2017-12-09 13:39:51 -0500

    Add basic animation for insertion of new images.
    - Refactor code to allow adding new thumbnail image.

--- LPThumbnailView/LPThumbnailView.swift
+++ LPThumbnailView/LPThumbnailView.swift
@@ -12,24 +12,20 @@ open class LPThumbnailView: UIView {
 
     // MARK: Members/Properties
 
-    public var image: UIImage? {
-        didSet { self.imageView.image = image }
-    }
+    public private(set) var images: [UIImage] = [UIImage]()
 
     public var imageScaleMode: UIViewContentMode = .scaleAspectFill {
         didSet { self.imageView.contentMode = self.imageScaleMode }
     }
 
-    public private(set) var imageCount: Int = 0
-
-    public var counterViewSize: CGFloat = 30.0 {
+    public var counterViewSize: CGFloat = 35.0 {
         didSet {
             self.counterViewHeightConstraint?.constant = self.counterViewSize
             self.counterViewWidthConstraint?.constant = self.counterViewSize
         }
     }
 
-    public var counterViewTopSpacing: CGFloat = 8.0 {
+    public var counterViewTopSpacing: CGFloat = 4.0 {
         didSet {
             self.counterViewTopConstraint?.constant = self.counterViewTopSpacing
         }
@@ -40,6 +36,8 @@ open class LPThumbnailView: UIView {
             self.counterViewTrailingConstraint?.constant = -self.counterViewTrailingSpacing
         }
     }
+
+    public var animationDuration: TimeInterval = 0.5
 
     private var counterViewTopConstraint: NSLayoutConstraint? = nil
 
@@ -66,20 +64,14 @@ open class LPThumbnailView: UIView {
 
     // MARK: Actions
 
-    public func updateImageCount(to newCount: Int, animated: Bool = true) {
-        self.imageCount = newCount
-        guard animated else { return }
-        UIView.transition(with: counterView.counterLabel,
-                          duration: 0.3, options: .transitionFlipFromBottom,
-                          animations: {
-                            self.counterView.counterLabel.text = "\(newCount)"
-                          },
-                          completion: nil
-        )
-    }
-
-    public func updateImageCount(by increment: Int, animated: Bool = true) {
-        self.updateImageCount(to: imageCount + increment, animated: animated)
+    public func addImage(_ img: UIImage, animated: Bool = true) {
+        self.images.append(img)
+        guard animated else {
+            self.imageView.image = img
+            self.counterView.counterLabel.text = "\(images.count)"
+            return
+        }
+        self.animateImageAddition(img)
     }
 
     // MARK: Helpers
@@ -110,6 +102,59 @@ open class LPThumbnailView: UIView {
         self.counterViewTrailingConstraint?.isActive = true
         self.counterViewHeightConstraint?.isActive = true
         self.counterViewWidthConstraint?.isActive = true
+    }
+
+    private func createTemporaryImageView(with image: UIImage) -> UIImageView {
+        let imgViewBounds = self.imageView.frame
+        let tempImageView = UIImageView()
+        tempImageView.frame = CGRect(x: imgViewBounds.origin.x + (imgViewBounds.width * 0.90) + 60,
+                                     y: imgViewBounds.origin.y,
+                                     width: imgViewBounds.width * 0.90,
+                                     height: imgViewBounds.height * 0.90)
+        tempImageView.clipsToBounds = true
+        tempImageView.contentMode = self.imageScaleMode
+        tempImageView.image = image
+        return tempImageView
+    }
+
+    private func animateImageAddition(_ img: UIImage) {
+        let tempImgView = createTemporaryImageView(with: img)
+        self.insertSubview(tempImgView, at: 0)
+
+        UIView.animateKeyframes(withDuration: animationDuration,
+                                delay: 0.0,
+                                options: [],
+                                animations: {
+                                    UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 1/3) {
+                                        tempImgView.frame = self.imageView.frame
+                                        tempImgView.layoutIfNeeded()
+                                    }
+                                    UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3) {
+                                        self.imageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                                        let trasitionDuration = self.animationDuration - (self.animationDuration * 1/3)
+                                        UIView.transition(with: self.imageView,
+                                                          duration: trasitionDuration * (1/3),
+                                                          options: .transitionCrossDissolve,
+                                                          animations: {
+                                                            self.imageView.image = img
+                                                          },
+                                                          completion: nil)
+                                        tempImgView.alpha = 0.0
+                                    }
+
+                                    UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3) {
+                                        UIView.transition(with: self.counterView.counterLabel,
+                                                          duration: self.animationDuration * (1/3),
+                                                          options: .transitionFlipFromBottom,
+                                                          animations: {
+                                                            self.counterView.counterLabel.text = "\(self.images.count)"
+                                                          },
+                                                          completion: nil)
+                                        self.imageView.transform = CGAffineTransform.identity
+                                    }
+                                },
+                                completion: nil
+        )
     }
 
     // MARK: Subviews
