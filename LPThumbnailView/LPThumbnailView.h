cf89f6fe24a6fa4e220b033e6e4b9eb01f15a060
Luis Padron <heyluispadron@gmail.com>
2017-12-08 22:54:28 -0500

    Initial Commit

---
+++ LPThumbnailView/LPThumbnailView.h
@@ -0,0 +1,19 @@
+//
+//  LPThumbnailView.h
+//  LPThumbnailView
+//
+//  Created by Luis Padron on 12/8/17.
+//  Copyright © 2017 Luis Padron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LPThumbnailView.
+FOUNDATION_EXPORT double LPThumbnailViewVersionNumber;
+
+//! Project version string for LPThumbnailView.
+FOUNDATION_EXPORT const unsigned char LPThumbnailViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LPThumbnailView/PublicHeader.h>
+
+
