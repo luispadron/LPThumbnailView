b05da8dc3aea750ed1cddd2e68bc31ac8edcc58a
Luis Padron <heyluispadron@gmail.com>
2018-04-09 13:04:24 -0400

    Move source files to src/ directory to avoid carthage build issues

--- run-jazzy.sh
+++ run-jazzy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jazzy --clean --output docs -a Luis Padron -u https://luispadron.com -m LPThumbnailView --module-version 1.0.0 -g https://github.com/luispadron/LPThumbnailView --theme apple
+jazzy --clean --source-directory src --output docs -a Luis Padron -u https://luispadron.com -m LPThumbnailView --module-version 1.0.0 -g https://github.com/luispadron/LPThumbnailView --theme apple
